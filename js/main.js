90aec629e704aa9c9bb00221b8415b46fbb328b0
taufik69 <taufik35-2915@diu.edu.bd>
2023-09-14 01:06:37 +0600

    our 2nd class

---
+++ js/main.js
@@ -0,0 +1,161 @@
+// ---> single line comment
+/*
+    multiline commnet
+*/
+
+// ----> js version  es4 , es5 , es6 , es7 , es8
+
+// variable
+
+// for, if else , funntion , class , constructor , method , object , array , new  let , var ,const ,
+
+// var nam = "all is well";
+// // var nam = "taufik";
+// console.log(nam);
+
+// let , const
+
+// var nam = "mohima";
+// var nam = "mahuya";
+// var nam = "shakil";
+// var nam = "redone";
+// var nam = "Nusrat";
+// var nam = "tamjid";
+
+// let nam = "mohima";
+// let nam = "mahuya";
+// let nam = "shakil";
+
+// var my = "Dhaka is our capital city";
+// 10 ----> browser  show hocce
+// 100 line code korcen
+
+// var my = "Noalhali is our capital city";
+//  12 ----> browser show korbe 12
+
+// es6 version arrived on let keyword that constains a value
+
+// let my = "Dhaka is our capital city";
+
+// let my = "Noalhali is our capital city";
+
+// const  . thats keywords are totally constant , decleared in single time  and assing value in single time
+
+// const x = "taufik";
+// x = "islam";
+
+// let x = "taufiik";
+// x = "islam";
+
+// var x = "tauifk";
+// var x = "islam";
+// x = "jodo";
+// x = "kodo";
+
+// console.log(x);
+
+// datatype on js
+
+// i) primitive datatype
+// ii) non primitive datatype
+
+// primitive datatype
+//  String --> "taufik" , 'islam' , `someting`
+// number ---> 1 ---> integer , 10.22----> float , 82934759234759823745897239845731298475423498752908234750984723905872349057234897569823475689 -----> bigInt
+// boolean  ---> true ---> false;
+// undefined --- > console.log(nam)
+// null ----> faka
+
+// nonprimitive datatypes
+
+// ----> array []
+// ----> object {}
+// ----> functon (){}
+
+// home work
+
+// 10 ta variable var ,let , const
+
+// var a = "taufik"  // this is string and also it is a primitve data type
+// let num = 1   // this is number
+// const bool = true  // this is boolean
+
+// var arr = []  //this is array and it is a non primitive data type
+
+// variable name convention
+
+var nam = "taufik";
+
+// 1st a number neya jabe na variable decleard korar somoi
+// var 1stVariable = "tau"
+// var $tafuik = "xyz"  ----> dont use speacila character in varable
+// var taufik-islam = "this is my name " don't use hypen and space in our variable,
+
+// var my_name_is_taufik = ""; ---> using underscor
+// myNameIsTaufik = "xyz" ---> camel casing variable declartion
+// var my82349759Name = "xyz" ---> this is collabarting variable naming or declaration
+
+// js equity comparison -->  , arithmatic operator , assignment operator , logical condition ,bitwise operator  , unary operator
+
+// 1st operator arthmatic operator ::
+
+// + ----> this operator called a plus  or addition
+// - ----> this is a minus or substraction
+// * ----> this is multiplication
+// /  ----> this is division
+// % ----> this is modulus
+// ** ---> this is expential
+
+// let a = 10;
+// let b = 20;
+// let sum = a ** 2;
+
+// console.log(sum);
+
+// -----> comparison oparator
+
+// ==  ---> 2 ta variable er maje compare kore , == shodho value ta eqal ba soman ase kina eita check kore.
+
+// === value and type 2 tai check kore
+// <  2 ta value er majhe konta and konta coto eita check kore
+// > 2 ta value er majhe konta and konta coto eita check kore
+// <= 2 ta value er majhe konta and konta coto eita check kore and soman kina eitao check kore
+// >= 2 ta value er majhe konta and konta coto eita check kore and soman kina eitao check kore <---------> !  ---> sonam noi
+
+// let a = 20;
+// let b = 10;
+
+// console.log(a != b);
+
+// assignment oparator
+
+// let a = 10;
+//  =
+// +=
+// -=
+// *=
+// /=
+
+// let (a = 10);    that's first bracket called a oparand
+// let a = 10;
+
+// console.log((a = a + 10));
+// console.log((a += 10));
+
+// console.log((a = a - 1));
+// console.log((a -= 1));
+
+// logical operator
+
+//  &&  and logical oparator  , jodi 2 ta ba er akadik trem true tahole se true hobe
+//  || and 2 tar modde 1 ta true holei se kaj korbe .
+
+// let age = false;
+// let job = false;
+
+// and && operator a (true and true) --> true (true && false) --> false (false && false) ---> false;
+// or || operator a (true and true) --> true (true && false) --> true (false && false) ---> false;
+
+// console.log(age && job);
+
+// 
